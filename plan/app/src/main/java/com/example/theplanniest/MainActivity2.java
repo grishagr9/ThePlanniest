65c861f71f5f2e75cf90754df105a02f6cd01429
grishagr9 <grisha-rnd10@yandex.ru>
2020-11-18 17:11:25 +0300

    18.11

--- plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
+++ plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
@@ -3,8 +3,10 @@ package com.example.theplanniest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity2 extends Activity {
@@ -13,6 +15,13 @@ public class MainActivity2 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_plan);
     }
+    public void Click_back(View view)
+    {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void Click_crate(View view)
+    {
 
-
+    }
 }
