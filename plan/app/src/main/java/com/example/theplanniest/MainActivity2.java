52348102e37db5608022b5595e97892cd1374313
grishagr9 <grisha-rnd10@yandex.ru>
2020-11-19 09:40:37 +0300

    19.11

--- plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
+++ plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.theplanniest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity2 extends Activity {
+    private Context MainActivityClass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +22,14 @@ public class MainActivity2 extends Activity {
     {
         Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
+        // TODO: 19.11.2020
+        //предупредить пользователя о том, что план не сохранится
     }
     public void Click_crate(View view)
     {
-
+        // TODO: 19.11.2020
+        //после нажатия кнопки создается кнопка этого плана на главном экране с надписью названия плана
     }
+
+
 }
