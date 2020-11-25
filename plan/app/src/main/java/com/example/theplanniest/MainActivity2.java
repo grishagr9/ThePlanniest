33d201e693e8d12ff0edd5f25f33521d71da70f1
grishagr9 <grisha-rnd10@yandex.ru>
2020-11-25 16:56:43 +0300

    нормальный ввод текста - готов

--- plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
+++ plan/app/src/main/java/com/example/theplanniest/MainActivity2.java
@@ -1,14 +1,19 @@
 package com.example.theplanniest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.app.Dialog;
+import android.content.DialogInterface;
 
 public class MainActivity2 extends Activity {
     private Context MainActivityClass;
@@ -20,16 +25,38 @@ public class MainActivity2 extends Activity {
     }
     public void Click_back(View view)
     {
+        //onCreateDialog(); вывод предупреждения о несохранённых данных
         Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
-        // TODO: 19.11.2020
-        //предупредить пользователя о том, что план не сохранится
     }
+
     public void Click_crate(View view)
     {
         // TODO: 19.11.2020
         //после нажатия кнопки создается кнопка этого плана на главном экране с надписью названия плана
     }
+    /*public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String title = "Предупреждение";
+        String message = "Если вы покинете страницу, введенные данные не сохранятся";
+        String button1String = "Отмена";
+        String button2String = "Покинуть страницу";
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);  // заголовок
+        builder.setMessage(message); // сообщение
+        builder.setPositiveButton(button1String, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
 
+            }
+        });
+        builder.setNegativeButton(button2String, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setCancelable(true);
+
+        return builder.create();
+    }*/
 }
